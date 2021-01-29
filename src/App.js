acb74c8554e32725dd0b19427f2dc8c4041a014d
Lexi Keen <lexikeen@Lexis-MacBook-Air.local>
2021-01-29 12:50:20 -0800

    setting up first three buttons and color variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Go ahead, push my buttons.</h2>
+        <row className="row">
+          <p>This is a row</p>
+        </row>
       </header>
+      <main className="main">
+        <row>
+          <button className="Button__primary">press me</button>
+          <button className="Button__secondary">press me</button>
+          <button className="Button__tertiary">press me</button>
+        </row>
+      </main>
     </div>
   );
 }
