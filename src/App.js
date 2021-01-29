bf8efbd1cb52008e6818a0c9df1fd8686e82af0a
Lexi Keen <lexikeen@Lexis-MacBook-Air.local>
2021-01-29 15:11:49 -0800

    playing with colors and styles and combining into one super button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import { Button } from "./components";
 
 function App() {
+  const handlePrimarySolidClick = () => {
+    alert("Solid primary button");
+  };
+  const handleSecondarySolidClick = () => {
+    alert("Solid secondary button");
+  };
+  const handleTertiarySolidClick = () => {
+    alert("Solid tertiary button");
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -11,10 +20,56 @@ function App() {
         </row>
       </header>
       <main className="main">
-        <row>
-          <button className="Button__primary">press me</button>
-          <button className="Button__secondary">press me</button>
-          <button className="Button__tertiary">press me</button>
+        <row className="row">
+          <Button
+            importance="primary"
+            label="primary button"
+            handleClick={handlePrimarySolidClick}
+          />
+          <Button
+            importance="secondary"
+            label="secondary button"
+            handleClick={handleSecondarySolidClick}
+          />
+          <Button
+            importance="tertiary"
+            label="tertiary button"
+            handleClick={handleTertiarySolidClick}
+          />
+        </row>
+        <row className="row">
+          <Button
+            importance="primary__outline"
+            label="primary button"
+            handleClick={handlePrimarySolidClick}
+          />
+          <Button
+            importance="secondary__outline"
+            label="secondary button"
+            handleClick={handleSecondarySolidClick}
+          />
+          <Button
+            importance="tertiary__outline"
+            label="tertiary button"
+            handleClick={handleTertiarySolidClick}
+          />
+        </row>
+        <row className="row">
+          <Button
+            importance="primary__text"
+            label="primary button"
+            handleClick={handlePrimarySolidClick}
+          />
+          <Button
+            importance="secondary__text"
+            label="secondary button"
+            handleClick={handleSecondarySolidClick}
+          />
+          <Button
+            importance="tertiary__text"
+            label="tertiary button"
+            handleClick={handleTertiarySolidClick}
+          />
         </row>
       </main>
     </div>
